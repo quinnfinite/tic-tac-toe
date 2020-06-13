d5dc6dad6b399b4e2b11bad2cb243e9fa16d4466
Quinn Royston <quinn.royston@gmail.com>
2020-06-12 17:12:01 -0700

    capitalized PORT process env variable

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
   //res.sendFile('index');
 })
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening at http://127.0.01:${port}`)
 });
